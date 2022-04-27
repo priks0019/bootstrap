c5ce1d9c07041ec25d998ebde0b56829d8b958d7
prikshitthakur <priks0019@gmail.com>
2022-04-27 17:29:11 +0530

    java script

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,17 @@
-var firstName = "prikshit";
-document.getElementById("hello").innerHTML = firstNmae;
+function global() {
+  let num1 = 89;
+  let num2 = 49;
+  function multi() {
+    return num1 * num2;
+  }
+  multi(); //IT WILL GIVE US 89*49
+  function add() {
+    let num1 = 69;
+    let num2 = 87;
+    function addition() {
+      return num1 + num2;
+    }
+    console.log(addition());
+  }
+  add();
+}
